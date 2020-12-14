87d86597a2ce3b4711f76fff35cf157a2786f29e
Nathan Ramli <hezkyanatanael84@gmail.com>
2020-12-14 22:19:36 +0700

    add day 10 problem

---
+++ 2020/day-10/day-10-part-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define INPUT_OUTPUT
+
+std::vector<int> q;
+
+int main()
+{
+#ifdef INPUT_OUTPUT
+    freopen("../input/day-10.txt", "r", stdin);
+    freopen("../output/day-10-part-1.txt", "w", stdout);
+#endif
+    int x;
+    while (std::cin >> x)
+        q.push_back(x);
+    std::sort(q.begin(), q.end());
+
+    x = 0;
+
+    int _1st = 0,
+        _3rd = 0;
+
+    size_t len = q.size();
+    for(size_t i=0;i<len;i++){
+        if(q[i] - x <= 3 && q[i] - x > 0){
+            if(q[i] - x == 1)
+                _1st++;
+            else if(q[i] - x == 3)
+                _3rd++;
+            x = q[i];
+        }
+    }
+    if(q[len - 1] == x)
+        _3rd++;
+    std::cout << _1st * _3rd << '\n';
+}
---
+++ 2020/day-10/day-10-part-2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+
+#define INPUT_OUTPUT
+
+std::vector<int> q;
+std::vector<int> adj[200];
+uint64_t dp[200];
+
+uint64_t ans = 0;
+int x,
+    target=0;
+
+uint64_t dfs(int a)
+{
+    uint64_t sm = 0;
+    if(a == target){
+        return 1;
+    }else {
+        for(int i = 0; i < adj[a].size(); i++){
+            if(dp[adj[a][i]]){
+                sm += dp[adj[a][i]];
+                continue;
+            }
+            uint64_t temp = dfs(adj[a][i]);
+            sm += temp;
+            dp[adj[a][i]] = temp;
+        }
+    }
+    return sm;
+}
+
+int main()
+{
+#ifdef INPUT_OUTPUT
+    freopen("../input/day-10.txt", "r", stdin);
+    freopen("../output/day-10-part-2.txt", "w", stdout);
+#endif
+    while (std::cin >> x){
+        q.push_back(x);
+        target = std::max(target, x);
+    }
+    std::sort(q.begin(), q.end());
+
+    x = 0;
+    int cnt = 1;
+    size_t len = q.size();
+    for(size_t j=0; j<len; j++){
+        for(size_t l=j; l<len; l++){
+            if(q[l] - x <= 3 && q[l] - x > 0){
+                adj[x].push_back(q[l]);
+            }else if(q[l] - x > 3)
+                break;
+        }
+        if(adj[x].size())
+            cnt *= adj[x].size();
+        x = q[j];
+    }
+    memset(dp, 0, 200);
+    std::cout << dfs(0) << '\n';
+}
